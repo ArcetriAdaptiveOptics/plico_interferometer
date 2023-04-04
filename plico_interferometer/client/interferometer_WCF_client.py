ae2a50ac415e3e1ce1bbb196f2076213298480aa
Lorenzo Busoni <lbusoni@gmail.com>
2023-04-04 12:55:21 +0200

    fixing interface return values

--- plico_interferometer/client/interferometer_WCF_client.py
+++ plico_interferometer/client/interferometer_WCF_client.py
@@ -3,8 +3,13 @@ from plico_interferometer.client.abstract_interferometer_client import \
 from plico_interferometer.devices.WCF_interface_for_4SightFocus import \
     WCFInterfacer
 from plico.utils.logger import Logger
-from plico.utils.decorator import override, returns
+from plico.utils.decorator import override
 import numpy as np
+from plico_interferometer.types.interferometer_status import \
+    InterferometerStatus
+from plico.utils.snapshotable import Snapshotable
+from plico_interferometer.utils.timeout import Timeout
+
 
 class InterferometerWCFClient(AbstractInterferometerClient):
 
@@ -30,7 +35,7 @@ class InterferometerWCFClient(AbstractInterferometerClient):
         ----------
         how_many: int
             numbers of frame to acquire
-        
+
         Returns
         -------
         masked_ima: numpy masked array
@@ -39,13 +44,15 @@ class InterferometerWCFClient(AbstractInterferometerClient):
         if how_many == 1:
             width, height, pixel_size_in_microns, data_array = \
                 self._i4d.take_single_measurement()
-            masked_ima = self._fromDataArrayToMaskedArray(width, height, data_array*632.8e-9)
+            masked_ima = self._fromDataArrayToMaskedArray(
+                width, height, data_array * 632.8e-9)
         else:
             image_list = []
             for i in range(how_many):
                 width, height, pixel_size_in_microns, data_array = \
                     self._i4d.take_single_measurement()
-                masked_ima = self._fromDataArrayToMaskedArray(width, height, data_array*632.8e-9)
+                masked_ima = self._fromDataArrayToMaskedArray(
+                    width, height, data_array * 632.8e-9)
                 image_list.append(masked_ima)
             images = np.dstack(image_list)
             masked_ima = np.ma.mean(images, axis=2)
@@ -63,10 +70,11 @@ class InterferometerWCFClient(AbstractInterferometerClient):
     @override
     def status(self):
         serial_number = self._i4d.get_system_info()
-        return serial_number
+        return InterferometerStatus(serial_number)
 
-    def snapshot(self, prefix='MY_4D'):
-        self.logger.notice("Getting snapshot for %s " % prefix)
-        serial_number = self._i4d.get_system_info()
-        return prefix, serial_number
-
+    @override
+    def snapshot(self,
+                 prefix,
+                 timeout_in_sec=Timeout.GETTER):
+        self._logger.notice("Getting snapshot for %s " % prefix)
+        return Snapshotable.prepend(prefix, self.status().as_dict())
--- setup.py
+++ setup.py
@@ -39,19 +39,19 @@ class UploadCommand(Command):
 
     def run(self):
         try:
-            self.status('Removing previous builds…')
+            self.status('Removing previous builds...')
             rmtree(os.path.join(here, 'dist'))
         except OSError:
             pass
 
-        self.status('Building Source and Wheel (universal) distribution…')
+        self.status('Building Source and Wheel (universal) distribution...')
         os.system(
             '{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
 
-        self.status('Uploading the package to PyPI via Twine…')
+        self.status('Uploading the package to PyPI via Twine...')
         os.system('twine upload dist/*')
 
-        self.status('Pushing git tags…')
+        self.status('Pushing git tags...')
         os.system('git tag v{0}'.format(about['__version__']))
         os.system('git push --tags')
 
@@ -64,7 +64,6 @@ setup(name=NAME,
       classifiers=['Development Status :: 4 - Beta',
                    'Operating System :: POSIX :: Linux',
                    'Programming Language :: Python :: 3',
-                   'Programming Language :: Python :: 2.6',
                    'Programming Language :: Python :: 2.7',
                    ],
       long_description=open('README.md').read(),
